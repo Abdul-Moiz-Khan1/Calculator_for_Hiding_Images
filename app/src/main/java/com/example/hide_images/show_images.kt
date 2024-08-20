ae21940f98728c548f3fb222acca712b7255b86e
Abdul-Moiz-Khan1 <moizz1326@gmail.com>
2024-08-20 15:43:39 +0500

    adding functionality of hiding and viewing images

--- app/src/main/java/com/example/hide_images/show_images.kt
+++ app/src/main/java/com/example/hide_images/show_images.kt
@@ -14,24 +14,27 @@ import com.example.hide_images.adapter.Image_Adapter
 import java.io.File
 
 class show_images : AppCompatActivity() {
-    private val imagePath = File("/storage/emulated/0/Android/data/com.example.hide_images/files/.app_images")
+    private val imagePath =
+        File("/storage/emulated/0/Android/data/com.example.hide_images/files/.app_images")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_images)
         window.statusBarColor = ContextCompat.getColor(this, R.color.black)
 
         val uri_list = intent.getParcelableArrayListExtra<Uri>("uri_list")
-        if(uri_list.isNullOrEmpty()){
-            Log.d("null? list" , "ahh shit null exception again")
+        if (uri_list.isNullOrEmpty()) {
+            Log.d("null? list", "ahh shit null exception again")
         }
-        for(i in 0 until uri_list!!.size){
-            Log.d("uri list" , uri_list[i].toString())
+        for (i in 0 until uri_list!!.size) {
+            Log.d("uri list", uri_list[i].toString())
         }
 
         val recyclerView = findViewById<RecyclerView>(R.id.rec_view)
-        recyclerView.layoutManager = GridLayoutManager(this,2)
-        val imageFiles = imagePath.listFiles { file -> file.extension == "jpg" || file.extension == "png" || file.extension == "jpeg" }
-        recyclerView.adapter = Image_Adapter(this , imageFiles)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        val imageFiles =
+            imagePath.listFiles { file -> file.extension == "jpg" || file.extension == "png" || file.extension == "jpeg" }
+        recyclerView.adapter = Image_Adapter(this, imageFiles)
 
     }
 
